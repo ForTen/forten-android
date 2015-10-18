cf7710c54b4f74a2a8115ca0432b95891d42785b
Minsoo Park <minsoo1003@gmail.com>
2015-10-19 04:25:12 +0900

    MainActivity 디자인 변경

--- app/src/main/kotlin/com/forten/android/activities/MainActivity.kt
+++ app/src/main/kotlin/com/forten/android/activities/MainActivity.kt
@@ -2,49 +2,28 @@ package com.forten.android.activities
 
 import android.app.Activity
 import android.os.Bundle
-import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import com.forten.android.R
-import com.forten.android.adapters.FeedAdapter
 import com.forten.android.networks.models.Post
-import com.forten.android.views.MainActionBar
+import com.forten.android.views.TimelineView
 import java.util.*
 
 class MainActivity : Activity() {
 
-    private lateinit var rvTimeline: RecyclerView
+    private lateinit var timeline: TimelineView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        actionBar.customView = MainActionBar(this)
-        actionBar.setDisplayShowTitleEnabled(false)
-        actionBar.setDisplayShowCustomEnabled(true)
+        timeline = findViewById(R.id.timeline) as TimelineView
 
-        rvTimeline = findViewById(R.id.rv_timeline) as RecyclerView
+        val posts = ArrayList<Post>()
+        for (i in 0..69) {
+            val post = Post()
+            post.body = "나랏말싸미듕긕에달아"
+            posts.add(post)
+        }
 
-        initializeRecyclerView()
-        loadData()
-    }
-
-    private fun initializeRecyclerView() {
-        val llm = LinearLayoutManager(this)
-        llm.orientation = LinearLayoutManager.VERTICAL
-
-        rvTimeline.setHasFixedSize(false)
-        rvTimeline.layoutManager = llm
-    }
-
-    private fun loadData() {
-        val list = ArrayList<Post>()
-        val feed = Post()
-        feed.body = "이것은테스트데이터다"
-        list.add(feed)
-        val feed2 = Post()
-        feed2.body = "Lorem ipsum dolor sit amet"
-        list.add(feed2)
-        val adapter = FeedAdapter(this, list)
-        rvTimeline.adapter = adapter
+        timeline.updateTimeline(posts)
     }
 }
